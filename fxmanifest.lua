9d5b6e67aa01486f90a13f8f25677569b9b531a0
kats1337 <70921605+kats1337@users.noreply.github.com>
2026-01-17 16:02:41 +0200

    init

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'kats'
+description 'Cooldown handler for Hellcity'
+
+
+shared_scripts {
+    '@ox_lib/init.lua',
+}
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server.lua'
+}
---
+++ server.lua
@@ -0,0 +1,135 @@
+identifierCooldowns = {}
+cdExpireEvents = {}
+
+---@param name string
+---@param time number (in seconds)
+AddCooldownForIdentifier = function(name, time)
+    if identifierCooldowns[name] then
+        print("Cooldown already exists for identifier: " .. name)
+        return false
+    end
+    identifierCooldowns[name] = os.time() + time
+    return true
+end
+
+---@param name string
+---@param time number (in seconds)
+ExtendCooldownForIdentifier = function(name, time)
+    if not identifierCooldowns[name] then
+        print("No existing cooldown for identifier: " .. name)
+        return false
+    end
+    identifierCooldowns[name] = identifierCooldowns[name] + time
+    return true
+end
+
+---@param name string
+RemoveCooldownForIdentifier = function(name)
+    if not identifierCooldowns[name] then
+        print("No existing cooldown for identifier: " .. name)
+        return false
+    end
+    identifierCooldowns[name] = nil
+    return true
+end
+
+---@param name string
+---@return boolean
+---@return number timeLeft (in seconds)
+IsIdentifierOnCooldown = function(name)
+    local currentTime = os.time()
+    local cooldownEndTime = identifierCooldowns[name]
+
+    if cooldownEndTime and cooldownEndTime > currentTime then
+        local timeLeft = cooldownEndTime - currentTime
+        return true, timeLeft
+    end
+
+    return false, 0
+end
+
+---@return table
+GetAllActiveCooldowns = function()
+    local currentTime = os.time()
+    local activeCooldowns = {}
+
+    for name, endTime in pairs(identifierCooldowns) do
+        if endTime > currentTime then
+            activeCooldowns[name] = endTime - currentTime
+        end
+    end
+
+    return activeCooldowns
+end
+
+---@param name string
+---@return number|nil endTime (timestamp)
+---@return nil
+GetCooldownEndTimeForIdentifier = function(name)
+    return identifierCooldowns[name]
+end
+
+---@param name string
+---@return number|nil startTime (timestamp)
+---@return nil
+GetCooldownStartTimeForIdentifier = function(name)
+    local endTime = identifierCooldowns[name]
+    if endTime then
+        local duration = endTime - os.time()
+        return endTime - duration
+    end
+    return nil
+end
+
+---@param name string
+---@return number|nil duration (in seconds)
+---@return nil
+GetCooldownDurationForIdentifier = function(name)
+    local endTime = identifierCooldowns[name]
+    if endTime then
+        local startTime = GetCooldownStartTimeForIdentifier(name)
+        return endTime - startTime
+    end
+    return nil
+end
+
+-- Loop every minute to clean up expired cooldowns
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(60000) -- 60 seconds
+        local currentTime = os.time()
+        for name, endTime in pairs(identifierCooldowns) do
+            if endTime <= currentTime then
+                identifierCooldowns[name] = nil
+                if cdExpireEvents[name] then
+                    local eventData = cdExpireEvents[name]
+                    TriggerEvent(eventData.event, table.unpack(eventData.args))
+                    cdExpireEvents[name] = nil
+                end
+            end
+        end
+    end
+end)
+
+exports('AddCooldownForIdentifier', function(name, time)
+    local res = GetInvokingResource()
+    cdExpireEvents[name] = {
+        event = res..':cooldownExpired',
+        args = {name},
+    }
+    AddCooldownForIdentifier(name, time)
+end)
+
+exports('RemoveCooldownForIdentifier', function(name)
+    if cdExpireEvents[name] then
+        cdExpireEvents[name] = nil
+    end
+    RemoveCooldownForIdentifier(name)
+end)
+
+exports('ExtendCooldownForIdentifier', ExtendCooldownForIdentifier)
+exports('IsIdentifierOnCooldown', IsIdentifierOnCooldown)
+exports('GetAllActiveCooldowns', GetAllActiveCooldowns)
+exports('GetCooldownEndTimeForIdentifier', GetCooldownEndTimeForIdentifier)
+exports('GetCooldownStartTimeForIdentifier', GetCooldownStartTimeForIdentifier)
+exports('GetCooldownDurationForIdentifier', GetCooldownDurationForIdentifier)
